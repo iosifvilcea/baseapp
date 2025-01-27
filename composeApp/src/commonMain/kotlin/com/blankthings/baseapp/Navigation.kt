5c7392e4ac7dcaa7f98838613f83f7b391e23119
Iosif Vilcea <iosif@blankthings.com>
2025-01-27 12:45:51 +0200

    Add login screen

--- composeApp/src/commonMain/kotlin/com/blankthings/baseapp/Navigation.kt
+++ composeApp/src/commonMain/kotlin/com/blankthings/baseapp/Navigation.kt
@@ -7,6 +7,10 @@ import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
@@ -19,7 +23,11 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import baseapp.composeapp.generated.resources.Res
 import baseapp.composeapp.generated.resources.account
+import baseapp.composeapp.generated.resources.create_account
+import baseapp.composeapp.generated.resources.forgot_password
 import baseapp.composeapp.generated.resources.home
+import baseapp.composeapp.generated.resources.login
+import baseapp.composeapp.generated.resources.onboarding
 import baseapp.composeapp.generated.resources.settings
 import com.blankthings.baseapp.ui.HomeScreen
 import com.blankthings.baseapp.ui.SettingsScreen
@@ -28,6 +36,10 @@ import org.jetbrains.compose.resources.stringResource
 
 enum class Screen(val title: StringResource, val icon: ImageVector) {
     Home(Res.string.home, Icons.Default.Home),
+    OnBoarding(Res.string.onboarding, Icons.Default.Face),
+    Login(Res.string.login, Icons.Default.AccountBox),
+    CreateAccount(Res.string.create_account, Icons.Default.Create),
+    ForgotPassword(Res.string.forgot_password, Icons.Default.AccountCircle),
     Account(Res.string.account, Icons.Default.Person),
     Settings(Res.string.settings, Icons.Default.Settings)
 }
@@ -36,19 +48,19 @@ enum class Screen(val title: StringResource, val icon: ImageVector) {
 fun BottomNavBar(navHostController: NavHostController) {
     BottomNavigation {
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Home, contentDescription = null) },
+            icon = { Icon(imageVector = Screen.Home.icon, contentDescription = Screen.Home.name) },
             selected = true,
             label = { Text(text = stringResource(Screen.Home.title)) },
             onClick = { navHostController.navigate(Screen.Home.name) }
         )
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Person, contentDescription = null) },
+            icon = { Icon(imageVector = Screen.Account.icon, contentDescription = Screen.Account.name) },
             selected = false,
             label = { Text(text = stringResource(Screen.Account.title)) },
             onClick = { navHostController.navigate(Screen.Account.name) }
         )
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Settings, contentDescription = null) },
+            icon = { Icon(imageVector = Screen.Settings.icon, contentDescription = Screen.Settings.name) },
             selected = false,
             label = { Text(text = stringResource(Screen.Settings.title)) },
             onClick = { navHostController.navigate(Screen.Settings.name) }
---
+++ composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/LoginScreen.kt
@@ -0,0 +1,71 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import baseapp.composeapp.generated.resources.BLANK
+import baseapp.composeapp.generated.resources.Res
+import baseapp.composeapp.generated.resources.password
+import baseapp.composeapp.generated.resources.username
+import com.blankthings.baseapp.Screen
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun LoginScreen(
+    navController: NavHostController = rememberNavController()
+) {
+    val blankString = stringResource(Res.string.BLANK)
+    val usernameString = stringResource(Res.string.username)
+    val passwordString = stringResource(Res.string.password)
+
+    val username = remember { mutableStateOf(blankString) }
+    val password = remember { mutableStateOf(blankString) }
+
+    Column(modifier = Modifier
+        .fillMaxHeight()
+        .padding(40.dp)) {
+
+        OutlinedTextField(
+            value = username.value,
+            onValueChange = { username.value = it },
+            leadingIcon = { Icon(Icons.Default.Person, contentDescription = usernameString) },
+            label = { Text(text = usernameString) },
+            modifier = Modifier.fillMaxWidth().padding(0.dp, 20.dp, 0.dp, 0.dp)
+        )
+
+        OutlinedTextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            leadingIcon = { Icon(Icons.Default.Info, contentDescription = passwordString) },
+            label = { Text(text = passwordString) },
+            modifier = Modifier.fillMaxWidth().padding(0.dp, 20.dp, 0.dp, 0.dp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        OutlinedButton(onClick = { },
+            modifier = Modifier.fillMaxWidth().padding(0.dp, 25.dp, 0.dp, 0.dp)) {
+            Text(
+                text = Screen.Login.name,
+                modifier = Modifier.fillMaxWidth().padding(5.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp
+            )
+        }
+    }
+}
