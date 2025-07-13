2cff44fc4ed1b3362578b0c615b07fb5d8db8664
iosifvilcea <iosifvilcea@protonmail.com>
2025-07-13 19:41:19 +0300

    init Network Monitor on iOS.

--- composeApp/src/iosMain/kotlin/com/blankthings/baseapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/blankthings/baseapp/MainViewController.kt
@@ -3,9 +3,12 @@ package com.blankthings.baseapp
 import androidx.compose.ui.window.ComposeUIViewController
 import com.blankthings.baseapp.datastore.DataStoreFactory
 import com.blankthings.baseapp.datastore.DataStoreManagerImpl
+import com.blankthings.baseapp.utils.NetworkMonitorFactory
+import com.blankthings.baseapp.utils.NetworkMonitorImpl
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController {
+    AppDependencies.initializeNetworkMonitor(NetworkMonitorImpl(NetworkMonitorFactory()))
     AppDependencies.initializeDataStore(DataStoreManagerImpl(DataStoreFactory()))
     return ComposeUIViewController {
         App()
