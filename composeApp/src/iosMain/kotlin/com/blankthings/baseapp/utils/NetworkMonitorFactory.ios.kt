35504fbd098e1a8592fa5dfb50c7f30be979f769
iosifvilcea <iosifvilcea@protonmail.com>
2025-07-13 19:31:21 +0300

    Add iOS implementation of network monitoring.

--- composeApp/src/iosMain/kotlin/com/blankthings/baseapp/utils/NetworkMonitorFactory.ios.kt
+++ composeApp/src/iosMain/kotlin/com/blankthings/baseapp/utils/NetworkMonitorFactory.ios.kt
@@ -1,7 +1,36 @@
 package com.blankthings.baseapp.utils
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import platform.Network.*
+import platform.darwin.dispatch_get_main_queue
+
 actual class NetworkMonitorFactory {
-    actual fun provideNetworkMonitor(): NetworkMonitor {
-        TODO("Not yet implemented")
+    actual fun provideNetworkMonitor(): NetworkMonitor = IosNetworkMonitor()
+}
+
+class IosNetworkMonitor : NetworkMonitor {
+    private val pathMonitor = nw_path_monitor_create()
+    private val _isOnline = MutableStateFlow(false)
+
+    override val isOnline: Flow<Boolean> = _isOnline.asStateFlow()
+
+    init {
+        startMonitoring()
+    }
+
+    private fun startMonitoring() {
+        nw_path_monitor_set_update_handler(pathMonitor) { path ->
+            val isConnected = nw_path_get_status(path) == nw_path_status_satisfied
+            _isOnline.value = isConnected
+        }
+
+        nw_path_monitor_set_queue(pathMonitor, dispatch_get_main_queue())
+        nw_path_monitor_start(pathMonitor)
+    }
+
+    fun stopMonitoring() {
+        nw_path_monitor_cancel(pathMonitor)
     }
 }
