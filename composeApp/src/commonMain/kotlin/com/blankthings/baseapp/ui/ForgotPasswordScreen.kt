f1033f493fb0c15f6ea59fdbfbacb7767f1af122
iosifvilcea <iosifvilcea@protonmail.com>
2025-07-15 11:56:42 +0300

    Move string literals in ForgotPass screen to strings.xml

--- composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/ForgotPasswordScreen.kt
+++ composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/ForgotPasswordScreen.kt
@@ -14,7 +14,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import baseapp.composeapp.generated.resources.Res
+import baseapp.composeapp.generated.resources.email_address
+import baseapp.composeapp.generated.resources.reset_password_prompt
 import com.blankthings.baseapp.component.BaOutlinedButton
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -27,7 +31,7 @@ fun ForgotPasswordScreen(onForgotPasswordClick: () -> Unit) {
     ) {
 
         Text(
-            text = "Enter your email address to reset your password",
+            text = stringResource(Res.string.reset_password_prompt),
             modifier = Modifier.fillMaxWidth()
         )
 
@@ -35,7 +39,7 @@ fun ForgotPasswordScreen(onForgotPasswordClick: () -> Unit) {
             value = email.value,
             onValueChange = { email.value = it },
             leadingIcon = { Icon(Icons.Default.Info, contentDescription = "") },
-            label = { Text(text = "Email Address") },
+            label = { Text(text = stringResource(Res.string.email_address)) },
             modifier = Modifier.fillMaxWidth().padding(0.dp, 20.dp, 0.dp, 0.dp)
         )
 
