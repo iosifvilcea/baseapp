32cbfca9d7fc79ed9c3c811151e30de2b11e61e7
iosifvilcea <iosifvilcea@protonmail.com>
2025-07-26 19:28:47 +0300

    Remove LoginRoute and move it's logic into their appropriate places - Routing to LoginNavigation and screen setup in LoginScreen.

--- composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/login/LoginNavigation.kt
+++ composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/login/LoginNavigation.kt
@@ -1,13 +1,18 @@
 package com.blankthings.baseapp.ui.login
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.blankthings.baseapp.data.AuthRepository
 import com.blankthings.baseapp.navigation.AppState
+import com.blankthings.baseapp.ui.home.navigateToHome
 import com.blankthings.baseapp.utils.ErrorType
 import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.stringResource
 
 @Serializable object LoginRoute
 
@@ -32,5 +37,35 @@ fun NavGraphBuilder.loginScreen(
             appState = appState,
             onShowSnackBar = onShowSnackBar
         )
+    }
+}
+
+@Composable
+fun LoginRoute(
+    loginViewModel: LoginViewModel,
+    appState: AppState,
+    onShowSnackBar: (ErrorType, String) -> Unit
+) {
+    val uiState by loginViewModel.uiState.collectAsStateWithLifecycle()
+
+    when (val currentState = uiState) {
+        is AuthUiState.Success -> {
+            loginViewModel.reset()
+            appState.navController.navigateToHome()
+        }
+        is AuthUiState.Failure -> {
+            loginScreen(
+                loginViewModel = loginViewModel,
+                showLoadingScreen = false
+            )
+            val errorMessage = stringResource(currentState.errorType.message)
+            onShowSnackBar.invoke(currentState.errorType, errorMessage)
+        }
+        else -> {
+            loginScreen(
+                loginViewModel = loginViewModel,
+                showLoadingScreen = currentState is AuthUiState.Loading
+            )
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/login/LoginScreen.kt
@@ -1,11 +1,15 @@
 package com.blankthings.baseapp.ui.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -13,7 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
@@ -30,6 +36,23 @@ import com.blankthings.baseapp.model.AuthData
 import com.blankthings.baseapp.utils.dismissibleKeyboard
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun loginScreen(
+    loginViewModel: LoginViewModel,
+    showLoadingScreen: Boolean = false
+) {
+    LoginScreen(onLoginClicked = loginViewModel::login)
+    if (showLoadingScreen) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.5f)), // Semi-transparent overlay
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(color = Color.White)
+        }
+    }
+}
 
 @Composable
 fun LoginScreen(onLoginClicked: (AuthData) -> Unit = { _ -> }) {
