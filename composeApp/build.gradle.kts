360c6be43af257fb85ce6b6612c90ce0b288d0ad
iosifvilcea <iosifvilcea@protonmail.com>
2025-07-28 21:33:12 +0300

    Add sqlite driver apis.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -57,6 +57,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.logging)
             implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.sqlite.bundled)
         }
     }
 }
