ac7a37aa34e72d088210ef8a184b70a0957c53ab
iosifvilcea <iosifvilcea@protonmail.com>
2025-07-30 15:57:07 +0300

    Change homeViewModel's uiState from collectAsState to collectAsStateWithLifecycle to make it lifecycle aware.

--- composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/home/HomeNavigation.kt
+++ composeApp/src/commonMain/kotlin/com/blankthings/baseapp/ui/home/HomeNavigation.kt
@@ -2,6 +2,7 @@ package com.blankthings.baseapp.ui.home
 
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -32,7 +33,7 @@ fun NavGraphBuilder.homeScreen(
         val viewModel: HomeViewModel = viewModel(
             factory = HomeViewModel.provideFactory(noteRepository)
         )
-        val uiState by viewModel.uiState.collectAsState()
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
         HomeScreen(uiState) { noteId ->
             onNoteClicked.invoke(noteId)
         }
